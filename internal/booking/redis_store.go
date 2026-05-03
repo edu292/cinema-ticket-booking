de2833eeab897fc158a2f9f62dcf5626b0cd22fd
edu292 <eduskoroboatei@gmail.com>
2026-05-03 09:34:22 -0300

    Implemented missing list bookings

--- internal/booking/redis_store.go
+++ internal/booking/redis_store.go
@@ -24,6 +24,21 @@ func sessionKey(id string) string {
 	return fmt.Sprintf("session:%s", id)
 }
 
+func parseSession(val string) (Booking, error) {
+	var data Booking
+	if err := json.Unmarshal([]byte(val), &data); err != nil {
+		return Booking{}, err
+	}
+
+	return Booking{
+		ID:      data.ID,
+		MovieID: data.MovieID,
+		SeatID:  data.SeatID,
+		UserID:  data.UserID,
+		Status:  data.Status,
+	}, nil
+}
+
 func (s *RedisStore) Book(b Booking) error {
 	_, err := s.hold(b)
 	if err != nil {
@@ -34,7 +49,27 @@ func (s *RedisStore) Book(b Booking) error {
 }
 
 func (s *RedisStore) ListBookings(movieID string) []Booking {
-	return []Booking{}
+	pattern := fmt.Sprintf("seat:%s:*", movieID)
+	var sessions []Booking
+
+	ctx := context.Background()
+
+	iter := s.rdb.Scan(ctx, 0, pattern, 0).Iterator()
+	for iter.Next(ctx) {
+		val, err := s.rdb.Get(ctx, iter.Val()).Result()
+		if err != nil {
+			continue
+		}
+
+		session, err := parseSession(val)
+		if err != nil {
+			continue
+		}
+
+		sessions = append(sessions, session)
+	}
+
+	return sessions
 }
 
 func (s *RedisStore) hold(b Booking) (Booking, error) {
